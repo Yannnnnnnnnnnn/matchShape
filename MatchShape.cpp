5af2f7de01a2b320a249804954562716ae1ba479
Yan <KevinYan37@163.com>
2017-11-28 15:43:29 +0800

    fix a error

--- MatchShape.cpp
+++ MatchShape.cpp
@@ -4,9 +4,9 @@
  * @author OpenCV team
  */
 
-//#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
 using namespace std;
